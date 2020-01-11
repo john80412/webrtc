06d3e3e1b2ef2e74ded9765f8b12788d6d433121
李日均 <lirijun@lirijundeMacBook-Pro.local>
2020-01-11 23:19:23 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/webrtc">webrtc</router-link>
     </div>
     <router-view/>
   </div>
 </template>
-
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
